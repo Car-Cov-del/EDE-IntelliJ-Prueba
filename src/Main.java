8c107c64a244931f42fa00caa1d758f4e30473f2
Carlos Coves <carcovdel@alu.edu.gva.es>
2023-10-23 16:50:17 +0200

    V2_Añadido algoritmo que detecta numero mayor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num1, num2;
         System.out.println("Hola Carlos :)");
+
+        System.out.println("Dime un número");
+        num1 = scan.nextInt();
+
+        System.out.println("Dime otro");
+        num2 = scan.nextInt();
+
+        if(num1<num2)
+            System.out.println("El número mayor es "+num2);
+
+        else
+            System.out.println("El número mayor es "+num1);
+
     }
 }
